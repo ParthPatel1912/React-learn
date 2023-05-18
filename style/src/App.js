d72b0800fb004d1024f837a84e29cbd8f6f3623c
ParthPatel1912 <60952527+ParthPatel1912@users.noreply.github.com>
2023-05-18 13:00:59 +0530

    style

---
+++ style/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import HeaderSass from './component/HeaderSass';
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <hr />
+            <hr />
+            <HeaderSass/>
+        </div>
+    );
+}
+
+export default App;
---
+++ style/src/component/HeaderSass.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../assets/css/custom.scss'
+
+export class HeaderSass extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Style!</h1>
+                <p>Add a little style!.</p>
+            </div>
+        )
+    }
+}
+
+export default HeaderSass
